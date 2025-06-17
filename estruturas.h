c8b08643791d4dacb197bc968155a5bdf2f45dcd
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-06-17 09:41:45 -0400

    adicionando função comecobra

--- estruturas.h
+++ estruturas.h
@@ -17,11 +17,12 @@ typedef struct position{
 //estrutura da cobra, tamanho e posiçao dela dentro do mapa
 typedef struct snake{
     position body[MAX];//armazena a posiçao da cobra dentro do jogo
-    int tamanho;
+    int tamanho;//tamanho do corpo da cobra
 }snake;
 
 void movimenta__cobra(snake* cobra, char direção);//funçao para captar a tecla digitada e mover a cobra
-void desenha_jogo();//desenha o mapa do jogo
+void desenha_jogo(snake* cobra);//desenha o mapa do jogo
 void desenha_cobra(snake* cobra);//desenha a cobra 
+int comecobra(snake* cobra);//função computa se a cobra comeu ou nao
 
 #endif //fecha o bloco
--- funcoes.c
+++ funcoes.c
@@ -17,24 +17,45 @@ void movimenta__cobra(snake* cobra, char direcao){//sobre o movimento da cobra
     }
 }
 
-void desenha_jogo(){//sobre o desenho do jogo
-        
+void desenha_jogo(snake* cobra){//sobre o desenho do jogo
+    
+    for (int y = 0; y < ALTURA; y++){
+        for(int x = 0; x < LARGURA; x++){
+           
+            if (y == 0 || y == ALTURA - 1){// imprime a primeira linha  e ultima
+                printf("#");
+            }
+
+            else if(x == 0 || x == LARGURA - 1){// imprime a primeira e ultima coluna
+                printf("#");
+            }
+
+            else{
+                desenha_cobra(cobra);
+                printf(" ");//imprime o meio do quadraro (ainda precisa de tratamento)
+            }
+        }
+        printf("\n"); //quebra a linha a cada vez que sai do segundo for
+    }
 }
 
 void desenha_cobra(snake* cobra){//sobre o desenho da cobra
     
     for (int y = 0; y < ALTURA; y++){
-        for (int x = 0; x < LARGURA; x++){
+        for (int x = 0; x < LARGURA; x++){//percorro sobre o mapa todo
+            
             int desenhou = 0;
 
-            if(cobra->body[0].x == x && cobra->body[0].y == y){
+            if(cobra->body[0].x == x && cobra->body[0].y == y){ //se a cabeça da cobra for encontrada eu a desenho
+               
                 printf("<8");//printando a cabeça da cobra
                 desenhou = 1;
             }
-
-            else{
+            else{//caso nao seja a cabeça desenha o corpo
+                
                 for(int i = 1; i < cobra->tamanho; i++){//percorre e desenha todo o corpo
-                    if(cobra->body[i].x ==x && cobra->body[i].y == y){
+                    
+                    if(cobra->body[i].x == x && cobra->body[i].y == y){
                         printf("o");//printando o corpo 
                         desenhou = 1;
                         break;
@@ -49,4 +70,5 @@ void desenha_cobra(snake* cobra){//sobre o desenho da cobra
         printf("\n");
     }
 }
+
 
