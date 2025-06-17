70a74b7c80edda62338fbf20439b1e925e4de137
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-06-16 20:22:05 -0400

    atualização da funçao jdesenha jogo

--- funcoes.c
+++ funcoes.c
@@ -18,16 +18,35 @@ void movimenta__cobra(snake* cobra, char direcao){//sobre o movimento da cobra
 }
 
 void desenha_jogo(){//sobre o desenho do jogo
-    return;
+        
 }
 
 void desenha_cobra(snake* cobra){//sobre o desenho da cobra
+    
     for (int y = 0; y < ALTURA; y++){
         for (int x = 0; x < LARGURA; x++){
+            int desenhou = 0;
+
             if(cobra->body[0].x == x && cobra->body[0].y == y){
-                printf("<8");
+                printf("<8");//printando a cabeça da cobra
+                desenhou = 1;
+            }
+
+            else{
+                for(int i = 1; i < cobra->tamanho; i++){//percorre e desenha todo o corpo
+                    if(cobra->body[i].x ==x && cobra->body[i].y == y){
+                        printf("o");//printando o corpo 
+                        desenhou = 1;
+                        break;
+                    }
+                }
+            }
+
+            if(!desenhou){
+                printf(" ");
             }
         }
+        printf("\n");
     }
 }
 
--- main.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main (){
-    printf("<0-");
-    printf(" -ooooooooo");
-    return 0;
-}
